2da5c6ce519181f3ab3df4867027736210966e3b
Michael Turner <mike@provable.com>
2026-05-18 20:12:45 -0400

    add swap action and bind on bridge client

--- packages/bridge/src/clients/decorators/bridge.ts
+++ packages/bridge/src/clients/decorators/bridge.ts
@@ -4,6 +4,7 @@ import { createOrder, type CreateOrderParameters, type CreateOrderReturnType } f
 import { getOrder, type GetOrderParameters, type GetOrderReturnType } from '../../actions/getOrder.js'
 import { getOrderAudit, type GetOrderAuditParameters, type GetOrderAuditReturnType } from '../../actions/getOrderAudit.js'
 import { waitForOrder, type WaitForOrderParameters, type WaitForOrderReturnType } from '../../actions/waitForOrder.js'
+import { swap, type SwapParameters, type SwapReturnType } from '../../actions/swap.js'
 
 export type BridgeActions = {
   getQuotes: (params: GetQuotesParameters) => Promise<GetQuotesReturnType>
@@ -11,6 +12,7 @@ export type BridgeActions = {
   getOrder: (params: GetOrderParameters) => Promise<GetOrderReturnType>
   getOrderAudit: (params: GetOrderAuditParameters) => Promise<GetOrderAuditReturnType>
   waitForOrder: (params: WaitForOrderParameters) => Promise<WaitForOrderReturnType>
+  swap: (params: SwapParameters) => Promise<SwapReturnType>
 }
 
 export function bridgeActions(client: Client): BridgeActions {
@@ -20,5 +22,6 @@ export function bridgeActions(client: Client): BridgeActions {
     getOrder: (params) => getOrder(client, params),
     getOrderAudit: (params) => getOrderAudit(client, params),
     waitForOrder: (params) => waitForOrder(client, params),
+    swap: (params) => swap(client, params),
   }
 }
