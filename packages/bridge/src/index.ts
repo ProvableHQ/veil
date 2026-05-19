0514e3c8a181996eaa85c895a96c4ea66aa13eab
Michael Turner <mike@provable.com>
2026-05-18 20:19:55 -0400

    export public surface from @veil/bridge index

--- packages/bridge/src/index.ts
+++ packages/bridge/src/index.ts
@@ -1,1 +1,84 @@
-export {}
+// Clients
+export {
+  createBridgeClient,
+  type BridgeClient,
+  type BridgeClientConfig,
+} from './clients/createBridgeClient.js'
+export { bridgeActions, type BridgeActions } from './clients/decorators/bridge.js'
+
+// Transport
+export { httpBridge } from './transports/httpBridge.js'
+
+// Actions (standalone forms)
+export {
+  getQuotes,
+  type GetQuotesParameters,
+  type GetQuotesReturnType,
+} from './actions/getQuotes.js'
+export {
+  createOrder,
+  type CreateOrderParameters,
+  type CreateOrderReturnType,
+} from './actions/createOrder.js'
+export {
+  getOrder,
+  type GetOrderParameters,
+  type GetOrderReturnType,
+} from './actions/getOrder.js'
+export {
+  getOrderAudit,
+  type GetOrderAuditParameters,
+  type GetOrderAuditReturnType,
+} from './actions/getOrderAudit.js'
+export {
+  waitForOrder,
+  type WaitForOrderParameters,
+  type WaitForOrderReturnType,
+} from './actions/waitForOrder.js'
+export {
+  swap,
+  type SwapParameters,
+  type SwapReturnType,
+} from './actions/swap.js'
+
+// Types
+export type {
+  ApiEnvelope,
+} from './types/envelope.js'
+export type {
+  BridgeIntegrationType,
+  BridgeOrderAuditDto,
+  BridgeOrderFinalStatus,
+  BridgeOrderFinalStatusReason,
+  BridgeOrderInstructions,
+  BridgeOrderProviderEventDto,
+  BridgeOrderStage,
+  BridgeOrderStatusDto,
+  BridgeOrderStepKey,
+  BridgeOrderStepStatus,
+  BridgeOrderStepStatusDto,
+  BridgeOrderTimelineEvent,
+  BridgeOrderTimelineEventStage,
+  BridgeQuote,
+  BridgeQuoteFeeEstimate,
+  BridgeQuoteFeeEstimateLeg,
+  BridgeQuoteMetadata,
+  DepositInstruction,
+  DepositInstructionType,
+  GetQuotesMeta,
+  ProviderStatusSnapshot,
+  ProviderSummary,
+  TerminalStage,
+} from './types/bridge.js'
+export { TERMINAL_STAGES, isTerminalStage } from './types/bridge.js'
+
+// Errors
+export {
+  BridgeError,
+  BridgeEnvelopeError,
+  BridgeOrderFailedError,
+  BridgeTimeoutError,
+} from './errors/bridgeErrors.js'
+
+// Utilities
+export { unwrapEnvelope } from './utils/unwrapEnvelope.js'
