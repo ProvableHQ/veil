17bb90fbcc43169ae059e13f5bc4e6d5b0e8c6ac
Michael Turner <mike@provable.com>
2026-05-18 20:07:35 -0400

    wire primitive actions into bridge decorator

--- packages/bridge/src/clients/decorators/bridge.ts
+++ packages/bridge/src/clients/decorators/bridge.ts
@@ -1,7 +1,24 @@
 import type { Client } from '@veil/core'
+import { getQuotes, type GetQuotesParameters, type GetQuotesReturnType } from '../../actions/getQuotes.js'
+import { createOrder, type CreateOrderParameters, type CreateOrderReturnType } from '../../actions/createOrder.js'
+import { getOrder, type GetOrderParameters, type GetOrderReturnType } from '../../actions/getOrder.js'
+import { getOrderAudit, type GetOrderAuditParameters, type GetOrderAuditReturnType } from '../../actions/getOrderAudit.js'
+import { waitForOrder, type WaitForOrderParameters, type WaitForOrderReturnType } from '../../actions/waitForOrder.js'
 
-export type BridgeActions = Record<string, never>
+export type BridgeActions = {
+  getQuotes: (params: GetQuotesParameters) => Promise<GetQuotesReturnType>
+  createOrder: (params: CreateOrderParameters) => Promise<CreateOrderReturnType>
+  getOrder: (params: GetOrderParameters) => Promise<GetOrderReturnType>
+  getOrderAudit: (params: GetOrderAuditParameters) => Promise<GetOrderAuditReturnType>
+  waitForOrder: (params: WaitForOrderParameters) => Promise<WaitForOrderReturnType>
+}
 
-export function bridgeActions(_client: Client): BridgeActions {
-  return {}
+export function bridgeActions(client: Client): BridgeActions {
+  return {
+    getQuotes: (params) => getQuotes(client, params),
+    createOrder: (params) => createOrder(client, params),
+    getOrder: (params) => getOrder(client, params),
+    getOrderAudit: (params) => getOrderAudit(client, params),
+    waitForOrder: (params) => waitForOrder(client, params),
+  }
 }
